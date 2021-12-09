56154fc874a959ec553e569b7cca9b5ef31b84c3
Harsh M <harshmaroo123@gmail.com>
2021-12-09 16:44:17 +0530

    Updated manjaro installers

--- For Manjaro/initialSetup.sh
+++ For Manjaro/initialSetup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
 #Script by misterblack0101 to install basic apps and setup for
-# MSI MODERN 14 on  
+# MSI MODERN 14 on
 #Manjaro
 
 cd ~
@@ -9,12 +9,18 @@ cd ~
 #UPDATE && UPGRADE
 echo "Inintializing script!!"
 
-
 echo "Updating mirrors"
 pacman-mirrors -g
 sudo pacman -Syy
-
-sudo pacman -Sy upgrade 
+echo
+echo "-------------------------------------------------------------------------"
+echo
+echo "Updating system"
+echo
+echo "-------------------------------------------------------------------------"
+echo
+pamac upgrade -a
+sudo pacman -Sy upgrade
 sudo pacman -Sy wget
 echo
 echo "-------------------------------------------------------------------------"
@@ -37,21 +43,20 @@ sudo pacman -Sy git
 echo
 echo
 read -p "Do you want to configure git username and email? y/n:  " ans
-if [ $ans = "y" ] || [ $ans = "Y" ]; 
-then
-read -p "Enter your username: " name
-echo
-read -p "Enter your email: " email
-echo
-git config --global user.email $email 
-git config --global user.name $name 
-git config --list
-echo
-echo "You can always change it by typing:"
-echo "git config --global user.email <email_id> "
-echo "git config --global user.name <name> "
-echo
-echo "All done!"
+if [ $ans = "y" ] || [ $ans = "Y" ]; then
+    read -p "Enter your username: " name
+    echo
+    read -p "Enter your email: " email
+    echo
+    git config --global user.email $email
+    git config --global user.name $name
+    git config --list
+    echo
+    echo "You can always change it by typing:"
+    echo "git config --global user.email <email_id> "
+    echo "git config --global user.name <name> "
+    echo
+    echo "All done!"
 fi
 echo
 echo "Git configured successfully!!"
@@ -61,7 +66,7 @@ echo
 #HTOP
 echo "Installing htop now"
 echo
-sudo pacman -Sy htop 
+sudo pacman -Sy htop
 echo "Htop installed successfully!!"
 echo
 echo "-------------------------------------------------------------------------"
@@ -74,7 +79,7 @@ echo
 # pacman -Sy update
 # sudo pacman -Sy install tlp tlp-rdw -y
 # read -p "Do you want to enable tlp, or wait for auto-cpufreq? (Suggested ans is N) y/n:  " ans
-# if [ $ans = "y" ] || [ $ans = "Y" ]; 
+# if [ $ans = "y" ] || [ $ans = "Y" ];
 # then
 # sudo systemctl enable tlp.service
 # sudo systemctl start tlp.service
@@ -87,10 +92,8 @@ echo
 #JAVA, PYTHON, C, C++
 echo "Installing jdk17 for java, python and c/c++ build essemtials now"
 echo
-sudo pacman -Sy openjdk-17-jre-headless 
-sudo pacman -Sy openjdk-17-jdk-headless  
-#sudo apt install python3.9 -y
-sudo pacman -Sy build-essential 
+sudo pamac install jdk
+sudo pacman -Sy build-essential
 echo "Compilers installed successfully!!"
 echo
 echo "-------------------------------------------------------------------------"
@@ -104,23 +107,18 @@ echo "-------------------------------------------------------------------------"
 #VSCODE
 echo "Installing VSCode now"
 echo
-flatpak install flathub com.visualstudio.code
+sudo pamac install code
 echo "VScode installed successfully!!"
 echo
 echo "-------------------------------------------------------------------------"
 echo
 
-
 #CHROME
-echo "Installing Google Chrome now"
+echo "Installing Google Chrome and MS-edge now"
 echo
-cd ~
-git clone https://aur.archlinux.org/google-chrome.git
-cd google-chrome
-sudo makepkg -Syi
-cd ~
-sudo rm -rf google-chrome
-echo "Google Chrome installed successfully!!"
+sudo pamac build google-chrome
+sudo pamac build microsoft-edge-stable-bin
+echo "Installed successfully!!"
 echo
 echo "-------------------------------------------------------------------------"
 echo
@@ -128,8 +126,8 @@ echo
 #MEDIA PLAYERS
 echo "Installing VLC and MPV now"
 echo
-sudo pacman -Sy vlc 
-sudo pacman -Sy mpv 
+sudo pacman -Sy vlc
+sudo pacman -Sy mpv
 echo "Media Players installed successfully!!"
 echo
 echo "-------------------------------------------------------------------------"
@@ -138,33 +136,17 @@ echo
 #Fonts and codecs
 echo "Installing fonts now"
 echo
-pamac upgrade -a
-pamac build ttf-ms-fonts 
+pamac build ttf-ms-fonts
 echo "-------------------------------------------------------------------------"
 echo
 
 #Torrent
 echo "Installing qbittorrent now"
 echo
-sudo pacman -Sy qbittorrent 
+sudo pacman -Sy qbittorrent
 echo "QBITTORRENT installed successfully!!"
 echo "-------------------------------------------------------------------------"
 echo
-
-#autocpufreq
-echo "Installing auto-cpufreq now"
-echo
-sudo pacman -Sy auto-cpufreq
-echo "autocpu-freq installed successfully!!"
-sudo systemctl enable auto-cpufreq
-sudo systemctl start auto-cpufreq
-
-echo "alias mon=\"auto-cpufreq --stats\"" >> .zshrc
-
-echo
-echo "auto-cpufreq has been enabled!"
-echo "-------------------------------------------------------------------------"
-
 
 #Torrent
 echo "Installing Discord now"
@@ -173,7 +155,6 @@ flatpak install flathub com.discordapp.Discord
 echo "Discord installed successfully!!"
 echo "-------------------------------------------------------------------------"
 
-echo "Install ms-edge manually"
+echo "Install auto-cpufreq manually"
 echo
 echo "Script has ended. Enjoy the setup!"
-
--- For Manjaro/iswFan.sh
+++ For Manjaro/iswFan.sh
@@ -1,53 +1,46 @@
 #!/usr/bin/env bash
 
 #Script by misterblack0101 to install fan control app for
-# MSI MODERN 14 on a 
+# MSI MODERN 14 on a
 #DEBIAN BASED OS
 
 #ISW for fans
 echo
 read -p "Do you want to  install MSI fan Systems? y/n:  " ans
-if [ $ans = "y" ] || [ $ans = "Y" ]; 
-then
-#----------------
-flag=0
-echo "Installing ISW for fans now"
-echo
-git clone https://github.com/YoyPa/isw
-cd isw
-sudo modprobe ec_sys write_support=1
-sudo install -Dm 644 etc/isw.conf "${pkgdir}/etc/isw.conf"
-sudo install -Dm 644 etc/modprobe.d/isw-ec_sys.conf "${pkgdir}/etc/modprobe.d/isw-ec_sys.conf"
-sudo install -Dm 644 etc/modules-load.d/isw-ec_sys.conf "${pkgdir}/etc/modules-load.d/isw-ec_sys.conf"
-sudo install -Dm 644 usr/lib/systemd/system/isw@.service "${pkgdir}/usr/lib/systemd/system/isw@.service"
-sudo install -Dm 755 isw "${pkgdir}/usr/bin/isw"
-echo
-echo "Fans will now start, and turn off after 15 secs..."
-sudo isw -b on
-sleep 15
-echo
-echo
+if [ $ans = "y" ] || [ $ans = "Y" ]; then
+    #----------------
+    cd ~
+    flag=0
+    echo "Installing ISW for fans now"
+    echo
+    git clone https://github.com/YoyPa/isw
+    cd isw
+    sudo modprobe ec_sys write_support=1
+    sudo install -Dm 644 etc/isw.conf "${pkgdir}/etc/isw.conf"
+    sudo install -Dm 644 etc/modprobe.d/isw-ec_sys.conf "${pkgdir}/etc/modprobe.d/isw-ec_sys.conf"
+    sudo install -Dm 644 etc/modules-load.d/isw-ec_sys.conf "${pkgdir}/etc/modules-load.d/isw-ec_sys.conf"
+    sudo install -Dm 644 usr/lib/systemd/system/isw@.service "${pkgdir}/usr/lib/systemd/system/isw@.service"
+    sudo install -Dm 755 isw "${pkgdir}/usr/bin/isw"
+    echo
+    echo "Fans will now start, and turn off after 15 secs..."
+    sudo isw -b on
+    sleep 15
+    echo
+    echo
 #---------------
 else
-echo "Not installing ISW fans, moving ahead"
-flag=1
+    echo "Not installing ISW fans, moving ahead"
+    flag=1
 fi
 echo "-------------------------------------------------------------------------"
 echo
 
-
-
-if [ $flag -eq 0 ];
-then
-sudo isw -b off
+if [ $flag -eq 0 ]; then
+    sudo isw -b off
 fi
 
 cd ~
 sudo rm -rf isw
-
-
-
-
 
 #  Setting aliases
 
@@ -56,17 +49,16 @@ echo "This is for zsh only. For any other shell, please set aliases manually."
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
 read -p "Do you want to set alias for controlling your fans? y/n:  " ans
-if [ $ans = "y" ] || [ $ans = "Y" ]; 
-then
-#----------------
-if
-echo "alias fan=\"sudo isw -b on\"" >> .zshrc
-echo "alias fanoff=\"sudo isw -b off\"" >> .zshrc
-echo
-echo "You can turn on/off the fans by using fan and fanoff respectively."
-echo "Restart your shell to see the changes."
+if [ $ans = "y" ] || [ $ans = "Y" ]; then
+    #----------------
+    cd ~
+    echo "alias fan=\"sudo isw -b on\"" >>.zshrc
+    echo "alias fanoff=\"sudo isw -b off\"" >>.zshrc
+    echo
+    echo "You can turn on/off the fans by using fan and fanoff respectively."
+    echo "Restart your shell to see the changes."
 
 #---------------
 else
-echo "Not setting aliases. Have a good day!"
+    echo "Not setting aliases. Have a good day!"
 fi
